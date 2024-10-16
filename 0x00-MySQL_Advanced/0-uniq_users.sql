4dee75cb4cdcbb3113379b7702c0114b2a26105d
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 01:13:03 +0300

    crete users table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- creating table users with
+-- the fields/attributes id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+	PRIMARY KEY(id)
+	);
