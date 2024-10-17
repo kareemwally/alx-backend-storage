8a9823017a8f5c3f8fd920a42ac7527d248aa21a
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 12:22:49 +0300

    group by origin and sum(fans)

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 -- selecting the most famous bands all over the globe
 -- based on their popularity
-SELECT origin , fans as nb_fans from metal_bands
+SELECT origin , SUM(fans) as nb_fans from metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
