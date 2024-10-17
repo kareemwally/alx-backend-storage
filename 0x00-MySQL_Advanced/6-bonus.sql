955087813e1f37388492678a6281f293b146fe91
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 20:02:17 +0300

    new correction

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,28 @@
 -- making a procedure to apply bonus on a certain project
 -- for a certain user
-delimiter $$
-create procedure AddBonus(user_id int, project_name varchar(255), score int)
-begin
-	select project_id into @project_id
-	from projects where projects.project_name = project_name;
+DELIMITER $$
 
-	IF @project_id IS NULL THEN
-        -- Insert the project into projects table if not found
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    DECLARE project_id INT;
+
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+
+    IF project_id IS NULL THEN
         INSERT INTO projects (name) VALUES (project_name);
-        -- Get the newly inserted project id
-        SET @project_id = LAST_INSERT_ID();
-	END IF;
-	update corrections set corrections.score = score 
-	where corrections.user_id = user_id and corrections.project_id = @project_id;
-end $$
-delimiter;
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    IF EXISTS (SELECT 1 FROM corrections WHERE user_id = user_id AND project_id = project_id) THEN
+        UPDATE corrections
+        SET score = score
+        WHERE user_id = user_id AND project_id = project_id;
+    ELSE
+        INSERT INTO corrections (user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+    END IF;
+END $$
+
+DELIMITER ;
