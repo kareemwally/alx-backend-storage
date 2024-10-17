8366eca164c61f9513091d9af42e857bbdbe0ba0
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 14:44:13 +0300

    longest Glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,11 @@
 -- selectin the longest and most consistent
 -- Glam rock band
-SELECT band_name, IFNULL(split - formed, 2022 - formed) AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    IFNULL(CAST(split AS UNSIGNED) - formed, 2022 - formed) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
