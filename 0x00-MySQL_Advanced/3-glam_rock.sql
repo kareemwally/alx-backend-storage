f58a010352df4251c8899d304546e1c5c2501af8
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 13:44:03 +0300

    glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
 -- selectin the longest and most consistent
 -- Glam rock band
-SELECT
-    band_name,
-    IFNULL(split - formed, 2022 - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+SELECT band_name, IFNULL(split - formed, 2022 - formed) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
