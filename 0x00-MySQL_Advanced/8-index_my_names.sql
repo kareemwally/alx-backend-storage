0bc4564d4744e7dfaba0798ee8aca1ee5f5f373f
kareem <kareemwally@std.mans.edu.eg>
2024-10-17 23:38:03 +0300

    index the 1st letter

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- creating index on the table names
 -- to speed up the search for large database
-CREATE INDEX idx_name_first ON names ((LEFT(name, 1)));
+CREATE INDEX idx_name_first ON names (name(1));
