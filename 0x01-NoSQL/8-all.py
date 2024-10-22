f96c1113363ec5e56b268b4c25280f4952413963
kareem <kareemwally@std.mans.edu.eg>
2024-10-23 00:22:31 +0300

    all documents in collection

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,10 +6,13 @@ a collection and returns all documents from a specified collection
 from pymongo import MongoClient
 
 
+def list_all(mongo_collection):
+    """
+    the monogo_collection that contains documents
+    """
+    res = list(mongo_collection.find())
+    return res
+
+
 if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """
-        the monogo_collection that contains documents
-        """
-        res = list(mongo_collection.find())
-        return res
+    list_all()
