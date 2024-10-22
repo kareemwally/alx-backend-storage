81a78b6a657339ab814c6f4ef57c7992ccf66723
kareem <kareemwally@std.mans.edu.eg>
2024-10-23 00:13:17 +0300

    all documents

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,16 +6,13 @@ a collection and returns all documents from a specified collection
 from pymongo import MongoClient
 
 
-def list_all(mongo_collection):
-    """
-    the monogo_collection that contains documents
-    """
-    try:
-        res = list(mongo_collection.find())
-        return res
-    except *:
-        print("can't connect to", mongo_collection)
-
-
 if __name__ == "__main__":
-    list_all()
+    def list_all(mongo_collection):
+        """
+        the monogo_collection that contains documents
+        """
+        try:
+            res = list(mongo_collection.find())
+            return res
+        except *:
+            print("can't connect to", mongo_collection)
