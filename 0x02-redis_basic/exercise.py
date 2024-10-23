befcb0605bb6248e729c9d7a864dc2bb31d207f4
kareem <kareemwally@std.mans.edu.eg>
2024-10-24 00:21:22 +0300

    store

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+a simple module to deal with redis by sending data
+in form of Key(bytes) :value(any data)
+"""
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """
+    the base class of the cashing system in project
+    """
+    def __init__(self):
+        """
+        instiating the connection with redis server on localhost
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, float, str, bytes]) -> uuid.UUID:
+        """
+        storing the specifed data whether it's int or float or string
+        or bytes
+        """
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
